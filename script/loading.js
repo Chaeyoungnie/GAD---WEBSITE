b3823125ce37e28ca16cd461e24f67c8c08f3144
= <=>
2025-11-17 16:08:52 +0800

    asd

--- script/loading.js
+++ script/loading.js
@@ -1,5 +1,5 @@
 // Set a delay of 3-5 seconds before redirecting
-const delay = Math.floor(Math.random() * 1000000) + 30000; // random between 3000-5000ms
+const delay = Math.floor(Math.random() * 2000) + 3000; // random between 3000-5000ms
 
 setTimeout(() => {
   window.location.href = "home.html";
